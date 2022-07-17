cfa42ecf58ef26cf86128b15d0bbb742779d6fb2
Adam MacDonald <am1macdonald@gmail.com>
2022-07-17 17:02:24 -0400

    - add clickHandler - add Grid component - add button for prototyping

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import Grid from "./components/GameGrid/Grid";
 import TopBar from "./components/TopBar/TopBar";
 
-
 function App() {
+  const [score, setScore] = useState(0);
+  // const [level, setLevel] = useState(1);
+
+  // const maxLevel = 5;
+
+  // const getRequiredScore = () => {
+  //   return level * 2;
+  // };
+
+  // const checkLevelComplete = () => {
+  //   // score >= getRequiredScore();
+  //   console.log(score >= getRequiredScore());
+  // };
+
+  const endGame = () => {
+    console.log("Game over!!");
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setScore(score + 1);
+    if (score === 10) {
+      endGame();
+    }
+  };
+
   return (
-    <TopBar title={"Memory Game"}/>
+    <div>
+      <TopBar
+        title={"Character Memory Game"}
+        score={score.toString()}
+        incrementScore={() => setScore(score + 1)}
+      />
+      <Grid handleClick={handleClick} />
+
+      <input
+        type="button"
+        name="incrementScore"
+        id="incrementScore"
+        value="Increment Score"
+        onClick={handleClick}
+      />
+    </div>
   );
 }
 
