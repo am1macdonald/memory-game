c87a4255be75159cbd02a4c1193a14006de692fb
am1macdonald <am1macdonald@gmail.com>
2022-07-21 13:02:33 -0400

    import stylesheets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './reset.css'
+import './stylesheets/reset.css'
+import './stylesheets/global.scss'
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
