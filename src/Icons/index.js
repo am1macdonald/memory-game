345c7c2d8f19f8b63607dec6063c2df1ef20203b
am1macdonald <am1macdonald@gmail.com>
2022-07-21 13:28:39 -0400

    create index file for icons

---
+++ src/Icons/index.js
@@ -0,0 +1,65 @@
+import beast from "./icons8-beast-500.png";
+import cyclops from "./icons8-cyclops-marvel-500.png";
+import deadpool from "./icons8-deadpool-500.png";
+import gambit from "./icons8-gambit-500.png";
+import jeanGrey from "./icons8-jean-grey-500.png";
+import magneto from "./icons8-magneto-500.png";
+import mystique from "./icons8-mystique-500.png";
+import professor from "./icons8-professor-x-500.png";
+import storm from "./icons8-storm-marvel-500.png";
+import wolverine from "./icons8-wolverine-500.png";
+
+const icons = [
+  {
+    src: beast,
+    alt: "beast",
+    id: "img01",
+  },
+  {
+    src: cyclops,
+    alt: "cyclops",
+    id: "img02",
+  },
+  {
+    src: deadpool,
+    alt: "deadpool",
+    id: "img03",
+  },
+  {
+    src: gambit,
+    alt: "gambit",
+    id: "img04",
+  },
+  {
+    src: jeanGrey,
+    alt: "jean grey",
+    id: "img05",
+  },
+  {
+    src: magneto,
+    alt: "magneto",
+    id: "img06",
+  },
+  {
+    src: mystique,
+    alt: "mystique",
+    id: "img07",
+  },
+  {
+    src: professor,
+    alt: "professor x",
+    id: "img08",
+  },
+  {
+    src: storm,
+    alt: "storm",
+    id: "img09",
+  },
+  {
+    src: wolverine,
+    alt: "wolverine",
+    id: "img10",
+  },
+];
+
+export default icons
