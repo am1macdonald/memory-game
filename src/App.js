2c56309df55140fdee0c5298d90e8cc7b86e74e5
Adam MacDonald <am1macdonald@gmail.com>
2022-07-21 18:40:33 -0400

    fix game logic

--- src/App.js
+++ src/App.js
@@ -1,51 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Grid from "./components/GameGrid/Grid";
 import TopBar from "./components/TopBar/TopBar";
 import BottomBar from "./components/BottomBar/BottomBar";
 import Rules from "./components/popups/Rules";
 import background from "./erik-mclean-27kCu7bXGEI-unsplash.jpg";
+import Announcement from "./components/popups/Announcement";
 
 function App() {
   const [score, setScore] = useState(0);
   const [map, setMap] = useState({});
   const [rulesDisplay, setRulesDisplay] = useState(true);
+  const [gameOver, setGameOver] = useState(false);
 
   const endGame = () => {
-    console.log(score === 10 ? "You win!" : "Game over!!");
     setScore(0);
     setMap({});
+    setGameOver(false);
   };
 
   const handleClick = (e) => {
     e.preventDefault();
-    setScore(score + 1);
-    if (
-      score === 10 ||
-      Object.prototype.hasOwnProperty.call(map, e.target.id)
+    if (Object.prototype.hasOwnProperty.call(map, e.target.id)
     ) {
-      endGame();
+      setGameOver(true);
+      return;
     }
+    setScore(score + 1);
     setMap((prevMap) => ({
       ...prevMap,
       [e.target.id]: undefined,
     }));
   };
+  useEffect(() => {
+    if (score === 10 ) {
+      setGameOver(true);
+      return;
+    }
+  })
 
   return (
-    <div className="app" style={{  
-      backgroundImage: `url(${background})`,
-      backgroundPosition: 'center',
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat'
-    }}>
+    <div
+      className="app"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       {rulesDisplay && <Rules toggle={() => setRulesDisplay(false)} />}
+      {gameOver && <Announcement restart={endGame} winOrLose={score === 10} />}
       <TopBar
-        title={"Character Memory Game"}
+        title={"Comic Card Game"}
         score={score.toString()}
         incrementScore={() => setScore(score + 1)}
       />
       <div className="main center">
-        {!rulesDisplay && <Grid handleClick={handleClick} />}
+        {!rulesDisplay && !gameOver && <Grid handleClick={handleClick} />}
       </div>
       <BottomBar />
     </div>
