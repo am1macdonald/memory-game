c9367564c5d7e2467c9ad01a5a6373d556f25567
Adam MacDonald <am1macdonald@gmail.com>
2022-07-21 19:02:20 -0400

    style bottom bar

--- src/components/BottomBar/BottomBar.js
+++ src/components/BottomBar/BottomBar.js
@@ -1,15 +1,24 @@
 import React from "react";
-import styles from "./BottomBar.module.scss"
+import styles from "./BottomBar.module.scss";
 
 function BottomBar() {
-
   return (
     <div className={`center footer ${styles.bottomBar}`}>
-      <a>Photo Credits</a>
-      Photo by <a href="https://unsplash.com/@introspectivedsgn?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Erik Mclean</a> on <a href="https://unsplash.com/photos/LSPUdV6IWsY?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-  
+      <span>
+        <a>Icon Credits</a>
+      </span>
+      <span className={styles.right}>
+        Background photo by{" "}
+        <a href="https://unsplash.com/@introspectivedsgn?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          Erik Mclean
+        </a>{" "}
+        on{" "}
+        <a href="https://unsplash.com/photos/LSPUdV6IWsY?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          Unsplash
+        </a>
+      </span>
     </div>
-  )
+  );
 }
 
-export default BottomBar
+export default BottomBar;
