9e54b6190eb80b9e5bccb60e6658fce76960c9dd
am1macdonald <am1macdonald@gmail.com>
2022-07-21 16:57:31 -0400

    add styles and photo credit

--- src/components/BottomBar/BottomBar.js
+++ src/components/BottomBar/BottomBar.js
@@ -1,10 +1,13 @@
 import React from "react";
+import styles from "./BottomBar.module.scss"
 
 function BottomBar() {
 
   return (
-    <div className="footer center">
+    <div className={`center footer ${styles.bottomBar}`}>
       <a>Photo Credits</a>
+      Photo by <a href="https://unsplash.com/@introspectivedsgn?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Erik Mclean</a> on <a href="https://unsplash.com/photos/LSPUdV6IWsY?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
     </div>
   )
 }
