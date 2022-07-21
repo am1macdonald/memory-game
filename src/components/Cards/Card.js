c678f7e49d6ebf47107c54e8c1a9f69259a571d4
am1macdonald <am1macdonald@gmail.com>
2022-07-21 12:14:03 -0400

    - add props to card

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -2,16 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Card.module.scss";
 
+
+
 function Card(props) {
+
   return (
-    <div className="card">
-      <img src={props.src} alt="error" />
+    <div id={props.id} className="card" onClick={props.handleClick}>
+      <img src={props.src} alt={props.alt} />
     </div>
   );
 }
 
 Card.propTypes = {
   src: PropTypes.string,
+  id: PropTypes.string,
+  alt: PropTypes.string,
+  handleClick: PropTypes.func
 };
 
 export default Card;
